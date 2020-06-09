865161f80b11c4caa343d88644b852a8ee0b1ea5
Jean Carlos Leon <jclv2004@gmail.com>
2020-06-09 10:40:19 -0500

    qr

--- app/index.js
+++ app/index.js
@@ -3,19 +3,30 @@ const router = require('express').Router();
 /**
  * Home page: muestra todos los ítems disponibles para comprar
  */
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
     res.render('home');
 });
 
 /**
  * Detail page: muestra la información del ítem seleccionado y permite crear la órden de pago
  */
-router.post('/detail', function (req, res) {
+router.post('/detail', function(req, res) {
 
     let data = req.body;
 
     //Base URL para correcta comunicación con el backend desde el frontend
-    data.baseurl = req.protocol+'://'+req.get('host');
+    data.baseurl = req.protocol + '://' + req.get('host');
+
+    res.render('detail', data);
+});
+
+
+router.post('/generarQR', function(req, res) {
+
+    let data = req.body;
+
+    //Base URL para correcta comunicación con el backend desde el frontend
+    data.baseurl = req.protocol + '://' + req.get('host');
 
     res.render('detail', data);
 });
